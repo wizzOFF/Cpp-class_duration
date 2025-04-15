d323f60b699d0e61bb445eaec800e26d1d850d9a
WizzOFF <maher_off@proton.me>
2025-04-15 17:05:17 +0200

    main.cpp: resultat = duree1 + duree2 + duree3

--- main.cpp
+++ main.cpp
@@ -5,14 +5,16 @@ using namespace std;
 
 int main()
 {
-    Duree duree1(0, 10, 42), duree2(0, 53, 27);
+    Duree duree1(0, 10, 42), duree2(0, 53, 27), duree3(0, 0, 10);
     Duree resultat;
 
     duree1.afficher();
     cout << "+" << endl;
     duree2.afficher();
+    cout << "+" << endl;
+    duree3.afficher();
 
-    resultat = duree1 + duree2;
+    resultat = duree1 + duree2 + duree3;
 
     cout << "=" << endl;
     resultat.afficher();
